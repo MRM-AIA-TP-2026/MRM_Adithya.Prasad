b0cd6c316e8cdf9494a67f42ce008d0a8cc1aa16
Adithya Prasad <adithyapgen15@gmail.com>
2024-10-29 10:58:54 +0530

    Add files via upload

---
+++ C++ Course_1/Week4/exercise3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+  
+  int a = stoi(argv[1]);
+  int b = stoi(argv[2]);
+  
+  if (a > b) {
+    int c = b;
+    b = a;
+    a = c;
+  }
+  
+  //add code below this line
+int sum = 0;
+int it = a;
+  
+while (it <= b) {
+  sum += it;
+  it++;
+}
+  
+if (a == b) {
+  sum = a;
+}
+  
+cout << sum << endl;
+
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
