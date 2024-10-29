d51de98c47ec4b0c18e02638059369e709d73c8d
Adithya Prasad <adithyapgen15@gmail.com>
2024-10-29 10:56:31 +0530

    Add files via upload

---
+++ C++ Course_1/Week2/exercise2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+int a = 5, b = 10;
+    cout << boolalpha << ((a >> 1) >= (b << 1));
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
