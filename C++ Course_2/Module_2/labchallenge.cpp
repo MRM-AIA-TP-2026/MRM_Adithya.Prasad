257e11dd892327feee0676bb00a82487b3694f64
Adithya Prasad <adithyapgen15@gmail.com>
2024-10-31 19:18:39 +0530

    Add files via upload

---
+++ C++ Course_2/Module_2/labchallenge.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int age1 = 12;
+  int age2 = 31;
+  int age3 = 29;
+  int* amy;
+  int* bob;
+  int** carol;
+  
+  //add code below this line
+  
+  
+  cout << *amy << endl; //do not edit
+  
+  cout << *bob << endl; //do not edit
+  
+  cout << **carol << endl; //do not edit
+  
+  
+  //add code above this line 
+  
+  return 0;
+  
+}
